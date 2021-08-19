eba3bf122ed17baddf7335099b2566cfba97af84
Paulo Eduardo Cruz de Lemos Junior <pauloeduardo2906@gmail.com>
2021-08-18 23:33:01 -0300

    created account class

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -0,0 +1,19 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Conta {
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+} {
+
+    fun deposito(valor: BigDecimal){
+
+    }
+
+    fun saque(valor: BigDecimal){
+
+    }
+
+}
